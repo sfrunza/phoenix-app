eb2aa34b7b665b7bf3c5b1e6eb8c3a1a1dbe69d4
Sergiu Frunza <frunza.sergiu3@gmail.com>
2022-04-04 17:22:37 -0400

    fixing

--- src/pages/account/requests/[id].js
+++ src/pages/account/requests/[id].js
@@ -1,49 +1,25 @@
 import React from 'react';
 import AccountJob from 'views/Account/Job';
+import { getSession } from 'next-auth/react';
 
 const AccountRequestPage = () => {
-  // const getCityState = (job, type) => {
-  //   let cityState = null;
-  //   if (type === 'origin') {
-  //     let origin = job.addresses.find((a) => a.isOrigin);
-  //     if (origin) {
-  //       cityState = origin.city + ', ' + origin.state;
-  //     }
-  //   } else if (type === 'destination') {
-  //     let destination = job.addresses.find((a) => a.isDestination);
-  //     if (destination) {
-  //       cityState = destination.city + ', ' + destination.state;
-  //     }
-  //   }
-  //   return cityState;
-  // };
-
-  // const o = getCityState(job, 'origin');
-  // const d = getCityState(job, 'destination');
-
   return <AccountJob />;
 };
 
-// AccountRequestPage.propTypes = {
-//   job: PropTypes.object.isRequired,
-// };
+export const getServerSideProps = async (context) => {
+  const session = await getSession(context);
 
-// export async function getStaticPaths() {
-//   const res = await fetch(`http://localhost:3000/api/jobs`);
-//   const data = await res.json();
-//   console.log(data)
-//   const paths = data.jobs.map((job) => ({
-//     params: { id: `${job.id}` },
-//   }));
-//   return { paths, fallback: false };
-// }
-
-// export async function getServerSideProps({ params }) {
-//   const id = params.id;
-
-//   const res = await fetch(`http://localhost:3000/api/jobs/${id}`);
-//   const job = await res.json();
-//   return { props: { params, job } };
-// }
+  if (!session) {
+    return {
+      redirect: {
+        destination: '/account/login',
+        permanent: false,
+      },
+    };
+  }
+  return {
+    props: { session },
+  };
+};
 
 export default AccountRequestPage;
